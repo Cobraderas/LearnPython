0cfb73247b67e3a6632991da3000cd33d1c0ad33
Cobraderas <catalinrock@gmail.com>
2019-07-13 20:47:13 +0300

    several commented loops

---
+++ Loops.py
@@ -0,0 +1,48 @@
+
+# count = 0
+
+# if count < 5:
+#     print("Hello, I am an if statement and count is", count)
+
+# while count < 5:
+#     print("Hello, I am a while and count is", count)
+#     count += 1
+
+
+# num = 1
+
+# while num <= 10:  # Fill in the condition
+#     print(num ** 2)  # Print num squared
+#     num += 1  # Increment num (make sure to do this!)
+
+# choice = input('Enjoying the course? (y/n)')
+
+# while choice != "y" and choice != "n":  # Fill in the condition (before the colon)
+#     choice = input("Sorry, I didn't catch that. Enter again: ")
+
+
+# count = 0
+
+# while True:
+#     print(count)
+#     count += 1
+#     if count >= 22:
+#         break
+
+
+# import random
+#
+# print("Lucky Numbers! 3 numbers will be generated.")
+# print("If one of them is a '5', you lose!")
+#
+# count = 0
+# while count < 3:
+#     num = random.randint(1, 6)
+#     print(num)
+#     if num == 5:
+#         print("Sorry, you lose!")
+#         break
+#     count += 1
+# else:
+#     print("You win!")
+
