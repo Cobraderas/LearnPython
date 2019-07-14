0c92155b7595b9d9e48dacc8ba7b64cf71208576
Cobraderas <catalinrock@gmail.com>
2019-07-14 14:44:55 +0300

    added several new loops

--- Loops.py
+++ Loops.py
@@ -66,8 +66,92 @@
 #     print("You lose!")
 
 
-print("Counting...")
+# print("Counting...")
+#
+# for i in range(20):
+#     print(i)
+#
+# hobbies = []
+#
+# # Add your code below!
+# for i in range(3):
+#     hobbi = input("Your hobbi is? :")
+#     hobbies.append(str(hobbi))
+# print(hobbies)
 
-for i in range(20):
-    print(i)
+
+# thing = "spam!"
+# for c in thing:
+#     print(c)
+# print("======")
+# word = "eggs!"
+#
+# # Your code here!
+# for a in word:
+#     print(a)
+
+
+# phrase = "A bird in the hand..."
+#
+# # Add your for loop
+# for char in phrase:
+#     if char == "A" or char == "a":
+#         print("X", end="")
+#     else:
+#         print(char, end="")
+#
+# # Don't delete this print statement!
+# print
+
+
+# numbers = [7, 9, 12, 54, 99]
+#
+# print("This list contains: ")
+#
+# for num in numbers:
+#     print(num)
+#
+# # Add your loop below!
+# for num in numbers:
+#     print(num ** 2)
+
+
+# d = {'a': 'apple', 'b': 'berry', 'c': 'cherry'}
+#
+# for key in d:
+#     # Your code here!
+#     print(key, d[key])
+
+
+# choices = ['pizza', 'pasta', 'salad', 'nachos']
+#
+# print('Your choices are:')
+# for index, item in enumerate(choices):
+#     print(index + 1, item)
+
+
+# # zip will create pairs of elements when passed two lists, and will stop at the end of the shorter list.
+# # zip can handle three or more lists as well!
+# list_a = [3, 9, 17, 15, 19]
+# list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+#
+# for a, b in zip(list_a, list_b):
+#   # Add your code here!
+#     if a > b:
+#         print(a)
+#     else:
+#         print(b)
+
+
+# # the else statement is executed after the for, but only if the for ends normally—that is, not with a break
+# fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+#
+# print('You have...')
+# for f in fruits:
+#     if f == 'tomato':
+#         print('A tomato is not a fruit!')  # (It actually is.)
+#         break
+#     print('A', f)
+# else:
+#     print('A fine selection of fruits!')
 
