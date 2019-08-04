f06b6820df09f832ef1adf59f32c06db67c7571b
Cobraderas <catalinrock@gmail.com>
2019-08-04 17:37:17 +0300

    added some code to  myToppics

--- myToppics.py
+++ myToppics.py
@@ -93,12 +93,26 @@
 # print(my_name[::-2])  # oinrTbo
 
 
+# If you don’t pass a particular index to the list slice, Python will pick a default.
+# The default starting index is 0.
+# The default ending index is the end of the list.
+# The default stride is 1.
+#
+# to_five = ['A', 'B', 'C', 'D', 'E']
+#
+# print(to_five[3:])
+# # prints ['D', 'E']
+#
+# print(to_five[:2])
+# # prints ['A', 'B']
+#
+# print(to_five[::2])
+# # print ['A', 'C', 'E']
 
 
-
-
-
-
+my_list = list(range(1, 11))
+backwards = my_list[::-1]
+print(backwards)
 
 
 
