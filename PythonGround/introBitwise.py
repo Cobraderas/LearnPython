3dcada9be64e92b720355dca4523425daf8073bf
Cobraderas <catalinrock@gmail.com>
2019-08-16 18:30:48 +0300

    added new exercises to introBitwise.py

--- PythonGround/introBitwise.py
+++ PythonGround/introBitwise.py
@@ -26,12 +26,108 @@
 # You can also represent numbers in base 8 and base 16 using the oct() and hex() functions.
 
 
-print(bin(1))
-print(bin(2))
-print(bin(3))
-print(bin(4))
-print(bin(5))
+# print(bin(1))
+# print(bin(2))
+# print(bin(3))
+# print(bin(4))
+# print(bin(5))
+#
+# print(hex(6))
+#
+# print(oct(7))
 
-print(hex(6))
 
-print(oct(7))
+#  ===========================================================================================================
+# int() function can turn non-integer input into an integer
+# int function has an optional second parameter
+# When given a string containing a number and the base that number is in,
+# the function will return the value of that number converted to base ten
+
+# print(int("1", 2))
+# print(int("10", 2))
+# print(int("111", 2))
+# print(int("0b100", 2))
+# print(int(bin(5), 2))
+# # Print out the decimal equivalent of the binary 11001001.
+# print(int("11001001", 2))
+
+
+# shift_right = 0b1100
+# shift_left = 0b1
+#
+# shift_right = shift_right >> 2
+# shift_left = shift_left << 2
+#
+# print(bin(shift_right))
+# print(bin(shift_left))
+
+
+#  bitwise AND (&) operator compares two numbers on a bit level and returns
+#  a number where the bits of that number are turned on if the corresponding
+#  bits of both numbers are 1
+#
+# print(bin(0b1110 & 0b101))
+# print(int("0b100", 2))
+
+
+# bitwise OR (|) operator compares two numbers on a bit level and returns a
+# number where the bits of that number are turned on if either of the
+# corresponding bits of either number are 1
+#
+# print(bin(0b1110 | 0b101))
+
+
+# XOR (^) or exclusive or operator compares two numbers on a bit level
+# and returns a number where the bits of that number are turned on
+# if either of the corresponding bits of the two numbers are 1, but not both
+#
+# print(bin(0b1110 ^ 0b101))
+
+
+# bitwise NOT operator (~) just flips all of the bits in a single number
+# print(~1)
+# print(~2)
+# print(~3)
+# print(~42)
+# print(~123)
+
+
+# def check_bit4(number):
+#     mask = 0b1000
+#     desired = number & mask
+#     if desired > 0:
+#         return "on"
+#     else:
+#         return "off"
+#
+#
+# print(check_bit4(0b0100))
+# print(check_bit4(0b1100))
+# print(check_bit4(0b1000))
+
+
+# You can also use masks to turn a bit in a number on using |.
+# For example, let’s say I want to make sure the rightmost bit of number a is turned on
+# a = 0b10111011
+# mask = 0b101111
+# desired = a | mask
+#
+# print(bin(desired))
+
+
+# Using the XOR (^) operator is very useful for flipping bits.
+# Using ^ on a bit with the number one will return a result where that bit is flipped
+# a = 0b11101110
+# mask = 0b11111111
+# desired = a ^ mask
+#
+# print(bin(desired))  # prints 0b10001
+
+
+# def flip_bit(number, n):
+#     mask = 0b1 << (n - 1)
+#     result = number ^ mask
+#     return bin(result)
+#
+#
+# print(flip_bit(0b10111, 1))
