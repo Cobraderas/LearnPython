aaf0e7901c7a04211bfce9513b4126c5bfbb6cfb
Cobraderas <catalinrock@gmail.com>
2019-08-31 22:11:05 +0300

    added comment on how to fix the connection issue and what was causing it

--- TestMySqlConnector.py
+++ TestMySqlConnector.py
@@ -15,6 +15,9 @@ from mysql.connector import errorcode
 
 # solving auth error
 # https://stackoverflow.com/questions/50557234/authentication-plugin-caching-sha2-password-is-not-supported
+# In MySQL 8.0, caching_sha2_password is the default authentication plugin rather than mysql_native_password
+# and passing auth_plugin='mysql_native_password' did not work,
+# because I accidentally installed mysql-connector instead of mysql-connector-python
 try:
     cnx = mysql.connector.connect(
         user=userMysql,
