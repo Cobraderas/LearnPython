f57479738fc5c44e941ea79e941859503a083559
Cobraderas <catalinrock@gmail.com>
2019-08-31 21:32:13 +0300

    added several new classes

--- PythonGround/TestMySqlConnector.py
+++ PythonGround/TestMySqlConnector.py
@@ -1,11 +1,15 @@
 # #if this page is executed with no errors, you have the "mysql.connector" module installed.
 import mysql.connector
 # from GeneralVariable import userMysql, passwdMysql
+# try:
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         passwd="Elmariachi100@"
+#     )
+# except:
+#     print("Connection Failed")
+#     mydb.close()
+# else:
+#     print(mydb)
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="Elmariachi100@"
-)
-
-print(mydb)
