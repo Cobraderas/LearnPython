9d0f897bf0e63db3c9cf42c7a02b27b260e50a20
Cobraderas <catalinrock@gmail.com>
2019-08-31 22:07:37 +0300

    connected with success to MySQLdb, failure was caused by insatlling the mysql-connector instead of mysql-connector-python

--- TestMySqlConnector.py
+++ TestMySqlConnector.py
@@ -1,6 +1,7 @@
 # #if this page is executed with no errors, you have the "mysql.connector" module installed.
-# import mysql.connector
-# from GeneralVariable import userMysql, passwdMysql
+import mysql.connector
+from GeneralVariable import userMysql, passwdMysql
+from mysql.connector import errorcode
 
 # mydb = mysql.connector.connect(
 #     host='127.0.0.1',
@@ -12,13 +13,15 @@
 # print(mydb)
 # mydb.close()
 
-import mysql.connector
-from mysql.connector import errorcode
-
+# solving auth error
+# https://stackoverflow.com/questions/50557234/authentication-plugin-caching-sha2-password-is-not-supported
 try:
-    cnx = mysql.connector.connect(user='root',
-                                  password='Elmariachi100@',
-                                use_pure=False)
+    cnx = mysql.connector.connect(
+        user=userMysql,
+        password=passwdMysql
+        )
+    print(cnx)
+    cnx.close()
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")
