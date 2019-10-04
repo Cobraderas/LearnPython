f9c0fa2611bb0ac220e8056c812b77fffb35d1c6
Cobraderas <catalinrock@gmail.com>
2019-10-04 18:11:56 +0300

    adding day 10 of course

--- PythonCourseGR/py_training_day10.py
+++ PythonCourseGR/py_training_day10.py
@@ -1,1 +1,39 @@
-#
+# iterators and generators
+
+# shift + F6 refactor and replace a name ex: iter1
+
+list1 = [i for i in range(1000)]
+
+print(sum(list1))
+
+iter1 = (i for i in range(1000))
+print(sum(iter1))
+# for i in iter1:
+#     print(i)
+
+
+class odd_nos:
+    def __init__(self, n):
+        self.n = n
+        self.num = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.num < self.n:
+            current_no = self.num
+            self.num += 2
+            return current_no
+        else:
+            raise StopIteration()
+
+
+for i in odd_nos(100):
+    print(i)
+
+
+
