d1e5b7b503b74bdbb0d8d34709ba36a76a2ce4fb
Cobraderas <catalinrock@gmail.com>
2019-10-18 17:55:15 +0300

    added day 14.a

--- PythonCourseGR/py_training_day14.py
+++ PythonCourseGR/py_training_day14.py
@@ -1,1 +1,20 @@
-#
+import re
+
+text = """re.search(pattern, string, flags=0)
+Scan through string looking for the first location where the regular expression pattern produces a match, and return a 
+corresponding match object. Return None if no position in the string matches the pattern; note that this is different 
+from finding a zero-length match at some point in the string.
+"""
+
+# regex101.com
+
+
+str1 = "[abc]"
+if str1.startswith("[") and str1.endswith("]"):
+    print(True)
+
+match = re.search(r"^\[.*\]$", str1)
+if match:
+    print(True)
+
+
