e5c5fbcc9fc08eb5dda98f5605c61ed4675e55de
Cobraderas <catalinrock@gmail.com>
2019-10-24 18:10:49 +0300

    added day 16 and payloads folder

--- py_training_day16.py
+++ py_training_day16.py
@@ -8,18 +8,58 @@ import os
 # os.system('calc')
 
 PAYLOADS_PATH = os.path.join(".", "payloads")
-FEATURE_X_PATH = os.path.join(PAYLOADS_PATH, "feature_y")
+FEATURE_X_PATH = os.path.join(PAYLOADS_PATH, "feature_x")
 FEATURE_Y_PATH = os.path.join(PAYLOADS_PATH, "feature_y")
 
-complete_p = subprocess.run(['payload.bat'],
-                            shell=True,
-                            cwd=FEATURE_X_PATH,
-                            capture_output=True,
-                            env={"user": "gradu",
-                                 "dut": "raspberry",
-                                 "cwd": FEATURE_X_PATH}
-                            )
-print(complete_p.returncode)
-print(complete_p.stdout)
-print(complete_p.stderr)
+# complete_p = subprocess.run(
+#                             ['payload.bat'],
+#                             shell=True,
+#                             cwd=FEATURE_X_PATH,
+#                             capture_output=True,
+#                             env={"user": "gradu",
+#                                  "dut": "raspberry",
+#                                  "cwd": FEATURE_X_PATH}
+#                             )
+
+
+# complete_p = subprocess.run(
+#                             ['payload.bat'],
+#                             shell=True,
+#                             cwd=FEATURE_Y_PATH,
+#                             capture_output=True,
+#                             env={"test_path": "c:\\"}
+#                             )
+# print(complete_p.returncode)
+# print(complete_p.stdout)
+# xxx = str(complete_p.stdout, encoding="UTF-8").splitlines()
+# for line in xxx:
+#     print(line)
+# print(complete_p.stderr)
+
+# subprocess.run("calc")
+
+import shlex
+# our_command = "dir c:\\windows"
+# our_command = r"explorer c:\\"
+#
+# args = shlex.split(our_command)
+# # subprocess.run(args, shell=True)
+# subprocess.run(args, shell=False)
+# print(args)
+
+
+mpopen = subprocess.Popen(["payload.bat"],
+                          shell=True,
+                          cwd=FEATURE_X_PATH,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          env={
+                              "user": "gradu",
+                              "dut": "raspberry",
+                              "cwd": FEATURE_X_PATH}
+                          )
+print(mpopen.poll())
+print(mpopen.communicate())
+print(mpopen.poll())
+print(mpopen.wait(1))
 
